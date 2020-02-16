ede5cf10f40a0cb27621287034573b30636b1d6f
Vineet Joshi <vineet0713@gmail.com>
2020-02-16 12:24:27 -0800

    Add components and basic UI.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe('Recipe 1', 'Description 1', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/01/Hummus-Recipe-20.jpg'),
+		new Recipe('Recipe 2', 'Description 2', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/01/Hummus-Recipe-20.jpg')
+	];
+
+	constructor() { }
+
+	ngOnInit() { }
+}
